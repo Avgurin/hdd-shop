0ca9aafb490a9e0fe060f2bdaac788d0ba7579b6
Denis Panechkin <avgurin@list.ru>
2026-06-11 19:23:56 +1100

    Post and User resource pages

---
+++ app/Models/Post.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Post extends Model
+{
+    protected $fillable = ['user_id', 'title', 'content', 'is_published'];
+
+    public function author(): BelongsTo
+    {
+        return $this->BelongsTo(User::class, 'user_id');
+    }
+}
